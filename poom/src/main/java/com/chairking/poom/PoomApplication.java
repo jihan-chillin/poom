5b2ad47792e7cdd5a095089ecedf6fe976ba9525
ChillingJihan <69563429+jihan-chillin@users.noreply.github.com>
2021-07-17 22:58:42 +0900

    start

---
+++ poom/src/main/java/com/chairking/poom/PoomApplication.java
@@ -0,0 +1,13 @@
+package com.chairking.poom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoomApplication.class, args);
+	}
+
+}
