bc7d336f258bdfe7e47764e5252299c645298128
Haeunlee1 <2leehaeun2@gmail.com>
2021-07-18 16:00:00 +0900

    하은-admin연결

--- src/main/java/com/chairking/poom/admin/controller/AdminController.java
+++ src/main/java/com/chairking/poom/admin/controller/AdminController.java
@@ -2,7 +2,10 @@ package com.chairking.poom.admin.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/admin")
@@ -12,4 +15,26 @@ public class AdminController {
 	public String admin() {
 		return "admin/admin_main";
 	}
+	
+	@GetMapping("/notice")
+	public ModelAndView notice(String type, ModelAndView mv) {
+		mv.addObject("type",type);
+		mv.setViewName("admin/admin_notice");
+		return mv;
+	}
+	
+	@GetMapping("/blame")
+	public ModelAndView blame(String type,ModelAndView mv) {
+		mv.addObject("type",type);
+		mv.setViewName("admin/admin_blame");
+		return mv;
+	}
+	
+	@GetMapping("/pay")
+	public ModelAndView pay(String type,ModelAndView mv) {
+		mv.addObject("type",type);
+		mv.setViewName("admin/admin_pay");
+		return mv;
+	}
+	
 }
