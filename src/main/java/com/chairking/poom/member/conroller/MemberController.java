b0615abf0373d32aaef81a983cf55c685da6688c
ChillingJihan <69563429+jihan-chillin@users.noreply.github.com>
2021-07-18 19:09:11 +0900

    jh - modi & mywrite

---
+++ src/main/java/com/chairking/poom/member/conroller/MemberController.java
@@ -0,0 +1,9 @@
+package com.chairking.poom.member.conroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+}
