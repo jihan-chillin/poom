3ca1e52bb8819d8b42cb77f8e0942e57b260670e
jinxging <82486622+jinxging@users.noreply.github.com>
2021-07-20 22:31:45 +0900

    회원가입 위치 받아오기(지오로케이션+카카오맵api) 구현 -jk

--- src/main/resources/static/js/login/signup_js_jk.js
+++ src/main/resources/static/js/login/signup_js_jk.js
@@ -1,60 +1,46 @@
-//약관동의 스크립트
-//모두 체크 or 해제
-function allCheck(a) {
-    if($(a).prop("checked")){
-        $(".nomal").prop("checked",true);
-        $("button.agree").prop("disabled",false).addClass('button_able');
-    }else {
-        $(".nomal").prop("checked",false); 
-        $("button.agree").prop("disabled",true).removeClass('button_able'); 
-    }
-};
-
-//아래 체크갯수에 따라 전체항목 체크여부 바꾸기
-function oneCheck(a) {
-    var allChkBox = $("#checkAll");
-    var chkBoxName = $(a).attr("class");
-
-    if($(a).prop("checked")) {
-        //전체체크박스 수(모두동의하기 체크박스 제외)
-        checkBoxLength = $("[class="+ chkBoxName +"]").length;
-        //체크된 체크박스 수
-        checkedLength = $("[class="+ chkBoxName +"]:checked").length;
-
-        if( checkBoxLength == checkedLength ) {
-            //전체체크박스수 == 체크된 체크박스 수 같다면 모두체크
-            allChkBox.prop("checked", true);
-            $("button.agree").prop("disabled",false).addClass('button_able');
-        }else {
-            allChkBox.prop("checked", false);
-            $("button.agree").removeClass('button_able');
-        }
-        
-    }else {
-        allChkBox.prop("checked", false);
-        
-    }
-};
-
-$(function(){
-    //모두동의하기 체크박스 클릭시
-    $("#checkAll").click(function(){
-            allCheck(this);
-    });
-    $(".nomal").each(function(){
-        $(this).click(function(){
-            oneCheck(this);
-        });
-    });
-});
-
-//약관동의 후 확인 클릭시 회원가입창으로 이동
-$("button.agree").click(function() {
-	location.replace("/signup");
-});
-
 //취소버튼 클릭시 메인페이지로 이동
 function goIndex() {
-	location.href="/";
+	location.replace("/");
 }
 
+//지역인증 -> 지역받아오기
+var options = {
+    enableHighAccuracy : true,
+    timeout : 5000,
+    maximumAge : 0
+};
+
+function success(pos) {
+	//위도,경도 받아오기
+    var crd = pos.coords;
+    lat = crd.latitude;
+    lon = crd.longitude;
+
+    getAddr(lat,lon);
+    function getAddr(lat,lon){
+        let geocoder = new kakao.maps.services.Geocoder();
+
+        let coord = new kakao.maps.LatLng(lat, lon);
+        let callback = function(result, status) {
+            if (status === kakao.maps.services.Status.OK) {
+                //지역명 받아오기
+                var locate = result[0].address.region_1depth_name;
+                console.log(locate);
+                $('input#location').val(locate);
+            }
+        };
+
+        geocoder.coord2Address(coord.getLng(), coord.getLat(), callback);
+    }
+    
+};
+
+function error(err) {
+    console.warn('ERROR(' + err.code + '): ' + err.message);
+};
+
+$("button#findLocation").click(function() {
+    console.log("되닝?");
+    navigator.geolocation.getCurrentPosition(success, error, options);
+
+})
