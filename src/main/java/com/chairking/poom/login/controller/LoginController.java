27eadf35e3dc4bd1c9b7cfef93e5a9ccfd6b2247
sehong0429 <qkrdlgo1214@naver.com>
2021-07-21 12:08:10 +0900

    gson에러 해결

--- src/main/java/com/chairking/poom/login/controller/LoginController.java
+++ src/main/java/com/chairking/poom/login/controller/LoginController.java
@@ -16,8 +16,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.google.gson.Gson;
 
+import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
