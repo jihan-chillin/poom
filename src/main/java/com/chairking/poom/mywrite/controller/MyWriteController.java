22100f58c2914b3bc831edfa8357b31d3f3cdc22
ChillingJihan <69563429+jihan-chillin@users.noreply.github.com>
2021-07-23 18:07:29 +0900

    jh : 이미지 삭제 js / 내가 쓴글 / 내가 쓴 댓글(수정해야함)

--- src/main/java/com/chairking/poom/mywrite/controller/MyWriteController.java
+++ src/main/java/com/chairking/poom/mywrite/controller/MyWriteController.java
@@ -1,29 +1,65 @@
 package com.chairking.poom.mywrite.controller;
 
+//import com.chairking.poom.mywrite.model.service.MywriteService;
+import com.chairking.poom.board.model.vo.Board;
+import com.chairking.poom.common.Pagination;
 import com.chairking.poom.mywrite.model.service.MywriteService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @Slf4j
 public class MyWriteController {
 
-//    @Autowired
-//    private MywriteService service;
+    @Autowired
+    private MywriteService service;
 
-//    // 내가 쓴 글
+    // 내가 쓴 글 리스트 가져오기
+    // 댓글수 넣는 거 수정해야 함.
     @GetMapping("/mywrite")
-    public String mywrite(){
-        return "/member/mywrite";
+    public ModelAndView mywrite( ModelAndView mv,
+            // cPage : 페이지바에 숫 자 몇 개?
+            @RequestParam(value = "cPage", defaultValue = "1") int cPage
+            ){
+            // 내가 쓴 글 개수
+            int totalData = service.countMyWrite();
+            // 한 페이지 당 띄울 글 개수
+            int numPerpage = 10;
+
+            List<Map<String, Object>> list = service.MywriteList(cPage, numPerpage);
+             // 1. 내가 쓴 글 리스트
+            mv.addObject("list", list);
+
+
+            // 2. 해당 글의 댓글 수
+            // int commentCount = service.commentCount();
+            mv.setViewName("/member/mywrite");
+            return mv;
     }
+
 
     // 내가 쓴 댓글
     @GetMapping("/mycomment")
-    public String mycomment(){
+    public String mycomment(ModelAndView mv,
+                            @RequestParam(value = "cPage", defaultValue = "1") int cPage)
+    {
+        // 내가 쓴 댓글 개수
+        int totalComment = service.countMyComment();
+        // 한 페이지 당 띄울 댓글 개수
+        int numPerpage = 10;
+
+        List<Map<String, Object>> cList = service.MyCommentList(cPage, numPerpage);
+
+        mv.addObject("cList", cList);
+
         return "/member/mycomment";
     }
 
