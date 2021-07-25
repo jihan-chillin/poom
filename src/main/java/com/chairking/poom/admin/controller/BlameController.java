e4231df67a5b0d8fff37cebb7c0f6a71e56e4cff
Haeunlee1 <2leehaeun2@gmail.com>
2021-07-25 14:21:25 +0900

    하은 / 팝업 form 데이터

--- src/main/java/com/chairking/poom/admin/controller/BlameController.java
+++ src/main/java/com/chairking/poom/admin/controller/BlameController.java
@@ -6,8 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -33,22 +33,31 @@ public class BlameController {
 	
 	//신고 팝업창
 	@RequestMapping(value="/report",method = {RequestMethod.GET, RequestMethod.POST})
-	@ResponseBody
-	public Map<String,String> insertBlame(@RequestParam Map<String,String> map,Model m) {
+	public ModelAndView report(@RequestParam Map<String,String> map,ModelAndView mv) {
 		//신고대상 값 보내야함
 		
 		System.out.println(map);
-		m.addAttribute("map", m);
+		mv.addObject("map", map);
 		
-		//mv.setViewName("common/blame_popup");
-		return map;
+		mv.setViewName("common/blame_popup");
+		return mv;
 	}
 	
-	@GetMapping("/report/page")
-	public String reportPage() {
-		
-		
-		return "common/blame_popup";
-	}
+//	@GetMapping("/report/page")
+//	public String reportPage() {
+//		
+//		
+//		return "common/blame_popup";
+//	}
 
+	@PostMapping("/insertblame")
+	public ModelAndView insertBlame(@RequestParam Map<String,String> map, ModelAndView mv) {
+			System.out.println("insertblame:"+map);
+			if(map.get("textarea").length()<1) {
+				System.out.println("textarea없음");
+			}else {
+				System.out.println("textarea있음");
+			}
+		return mv;
+	}
 }
