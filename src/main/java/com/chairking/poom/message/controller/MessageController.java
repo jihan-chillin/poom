bcba5985d066477f75c61e47eb16eab024caacf7
sehong0429 <qkrdlgo1214@naver.com>
2021-07-25 15:28:16 +0900

    이것 저것 구현 ..메세지ㅣㅜㅜㅜ웩웩 ajax, 삭제 등등

--- src/main/java/com/chairking/poom/message/controller/MessageController.java
+++ src/main/java/com/chairking/poom/message/controller/MessageController.java
@@ -4,6 +4,7 @@ package com.chairking.poom.message.controller;
 import com.chairking.poom.message.model.service.MessageService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 import java.util.Map;
 
@@ -21,16 +24,19 @@ public class MessageController {
 
     @Autowired
     private MessageService service;
-
+    @Autowired
+    PasswordEncoder pwEncoder;
     //쪽지함 탭 보이는 메인페이지
     @GetMapping()
     public String message(String type,Model m){
+        //System.out.println("pw: " + pwEncoder.encode("1234"));
         m.addAttribute("type",type);
         return "message/message_main";
     }
 
     @GetMapping("/main")
     public ModelAndView main(String type, ModelAndView mv){
+
         mv.addObject("type", type);
         mv.setViewName("message/message");
         return mv;
@@ -39,9 +45,15 @@ public class MessageController {
 
     //ajax 받은 쪽지 페이지
     @GetMapping("/receive")
-    public ModelAndView receiveMessage(String type, ModelAndView mv){
-        System.out.println(type);
-        List<Map<String,Object>> list = service.receiveMessage();
+    public ModelAndView receiveMessage(HttpServletRequest req, ModelAndView mv){
+        HttpSession session = req.getSession();
+        // 세션에서 내 아이디 가져옴
+        Map<String,String> val = (Map<String,String>)session.getAttribute("loginMember");
+        String myId = val.get("MEMBER_ID");
+
+        System.out.println("myId:" + myId);
+        String condition = "AND RECV_MEMBER = '" + myId + "' AND MSG_TYPE!=3";
+        List<Map<String,Object>> list = service.getMessage(condition);
         mv.addObject("list",list);
         System.out.println(list);
         mv.setViewName("message/message_receiveMessage");
@@ -52,10 +64,32 @@ public class MessageController {
 
     //ajax 보낸 쪽지 페이지
     @GetMapping("/send")
-    public ModelAndView sendMessage(ModelAndView mv){
-        List<Map<String,Object>>list = service.sendMessage();
+    public ModelAndView sendMessage(HttpServletRequest req, ModelAndView mv){
+        HttpSession session = req.getSession();
+        // 세션에서 내 아이디 가져옴
+        Map<String,String> val = (Map<String,String>)session.getAttribute("loginMember");
+        String myId = val.get("MEMBER_ID");
+
+        System.out.println("myId:" + myId);
+        String condition = "AND MEMBER_ID = '" + myId + "'";
+        List<Map<String,Object>>list = service.getMessage(condition);
         mv.addObject("list",list);
         mv.setViewName("message/message_sendMessage");
+        return mv;
+    }
+
+    //ajax 차단함
+    @GetMapping("/block")
+    public ModelAndView blockMessage(HttpServletRequest req, ModelAndView mv){
+        HttpSession session = req.getSession();
+        // 세션에서 내 아이디 가져옴
+        Map<String,String> val = (Map<String,String>)session.getAttribute("loginMember");
+        String myId = val.get("MEMBER_ID");
+
+        String condition = "AND RECV_MEMBER = '" + myId + "' AND MSG_TYPE=3";
+        List<Map<String,Object>>list = service.getMessage(condition);
+        mv.addObject("list",list);
+        mv.setViewName("message/message_blockMessage");
         return mv;
     }
 
@@ -94,11 +128,21 @@ public class MessageController {
     }
 
     //쪽지내용 팝업
-    @GetMapping("/message/content")
+    @GetMapping("/content")
     public String contentPopup(@RequestParam String msgNo, Model m){
         List<Map<String,Object>> list = service.messageContent(msgNo);
         m.addAttribute("list",list);
         return "message/message_content";
+    }
+
+    @GetMapping("/delete")
+    public String deleteMessage(@RequestParam String msgNo, Model m){
+        System.out.println("delete: " + msgNo);
+        int result = service.deleteMessage(msgNo);
+        m.addAttribute("result", result);
+
+
+        return "message/message_main";
     }
 
     //받는사람 검색
