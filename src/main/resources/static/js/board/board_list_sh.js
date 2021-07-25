af9be8baa6f7539894ae9a0343a901d203300461
ChillingJihan <69563429+jihan-chillin@users.noreply.github.com>
2021-07-25 16:41:20 +0900

    jh : 게시글 리스트 수정, 개인정보 스크롤 에러 수정, 게시글 작성파트 css 등

--- src/main/resources/static/js/board/board_list_sh.js
+++ src/main/resources/static/js/board/board_list_sh.js
@@ -1,14 +1,20 @@
-const writeBoard=()=>{
-	$.ajax({
-		url:"/board/form",
-		success:data=>{
-			$("div#content").html(data);
-		},
-		error:function(r,s,e){
-			console.log(r, s, e);
-		}
-	});
+// const writeBoard=()=>{
+// 	location.assign("/board/form")
+// 	// $.ajax({
+// 	// 	url:"/board/form",
+// 	// 	success:data=>{
+// 	// 		$("div#content").html(data);
+// 	// 	},
+// 	// 	error:function(r,s,e){
+// 	// 		console.log(r, s, e);
+// 	// 	}
+// 	// });
+// }
+
+const movetoForm=()=>{
+	location.assign("/board/form");
 }
+
 const viewBoard=()=>{
 	$.ajax({
 		url:"/board/view?boardNo=2",
