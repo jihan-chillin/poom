f688979ad971c7fe02cbc4b67993b15c2335d635
Sehee Kim <tpgmlee@naver.com>
2021-07-26 19:18:56 +0900

    sh-결제기능 수정

--- src/main/resources/static/js/pay/pay_sh.js
+++ src/main/resources/static/js/pay/pay_sh.js
@@ -1,5 +1,6 @@
 //결제
 function pay()	{
+	console.log("${loginMember}");
 	var IMP = window.IMP; // 생략가능
 	IMP.init('iamport'); // 'iamport' 대신 부여받은 "가맹점 식별코드"를 사용
 	// 'iamport' 대신 부여받은 "가맹점 식별코드"를 사용
