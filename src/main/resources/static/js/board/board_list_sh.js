1589dfbf42845a6fd47d5578d6038aa8040a36de
ChillingJihan <69563429+jihan-chillin@users.noreply.github.com>
2021-07-27 14:32:34 +0900

    jh : 게시글 리스트 오류 수정

--- src/main/resources/static/js/board/board_list_sh.js
+++ src/main/resources/static/js/board/board_list_sh.js
@@ -1,24 +1,13 @@
-// const writeBoard=()=>{
-// 	location.assign("/board/form")
-// 	// $.ajax({
-// 	// 	url:"/board/form",
-// 	// 	success:data=>{
-// 	// 		$("div#content").html(data);
-// 	// 	},
-// 	// 	error:function(r,s,e){
-// 	// 		console.log(r, s, e);
-// 	// 	}
-// 	// });
-// }
 
 // 전체 글 보기 ajax
 function allBoardList(){
 	$('.feed_write').remove();
 	$('.feed_new').remove();
+
 	$('.feed').css({
 		"background": "#f7f7f7","border-radius":"20px",
 		"height" : "800px"
-	})
+	});
 
 	$.ajax({
 		url:"/board/all",
@@ -34,39 +23,12 @@ function allBoardList(){
 	})
 }
 
+
 const movetoForm=()=>{
 	location.assign("/board/form");
 }
 
-// $("[name=boardDetail]").click((e)=>{
-//
-// 	$('#board-whole-content').remove();
-//
-// 	const boardNo = e.target.title;
-// 	$.ajax({
-// 		url:"/board/view",
-// 		data:{
-// 			"boardNo" : boardNo
-// 		}
-// 	}).done(function(result){
-// 		$(".feed").html(result);
-// 	});
-// });
 
-const viewBoard=()=>{
-
-	$('#board-whole-content').remove();
-
-	$.ajax({
-		url:"/board/view",
-		data:{
-			"boardNo" : boardNo
-		},
-		success:function(result){
-			$(".feed").html(result);
-		}
-	});
-}
 
 //글쓰기 버튼 클릭시 글작성 페이지로 이동
 function goWriteForm() {
