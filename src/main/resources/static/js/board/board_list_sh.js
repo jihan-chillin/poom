4d45ba5e337e667fae4309de244723467258053e
ChillingJihan <69563429+jihan-chillin@users.noreply.github.com>
2021-07-27 19:11:03 +0900

    jh : 글작성 에디터 api 진행중

--- src/main/resources/static/js/board/board_list_sh.js
+++ src/main/resources/static/js/board/board_list_sh.js
@@ -24,12 +24,6 @@ function allBoardList(){
 }
 
 
-const movetoForm=()=>{
-	location.assign("/board/form");
-}
-
-
-
 //글쓰기 버튼 클릭시 글작성 페이지로 이동
 function goWriteForm() {
 	$('.feed_write').remove();
@@ -47,4 +41,23 @@ function goWriteForm() {
       console.log(i);
     }
   });
+}
+
+// 게시판리스트에서 글작성 페이지로 이동 ajax
+function moveTooWriteForm(){
+	$('#board-list-catebar').remove();
+	$('#board_list-table').remove();
+	$('.feed').css({"background": "#f7f7f7","border-radius":"20px"});
+
+	$.ajax({
+		url:'/board/form',
+		success:function(data){
+			$('.feed').html(data)
+		},
+		error:(e,m,i)=>{
+			console.log(e);
+			console.log(m);
+			console.log(i);
+		}
+	});
 }
---
+++ src/main/resources/static/js/ckeditor/ckeditor.js
@@ -0,0 +1,8 @@
+CKEDITOR.replace('ckeditor-area',
+    { filebrowserUploadUrl: '/common/ckeditor/fileUpload',
+        font_names: '맑은 고딕/Malgun Gothic;굴림/Gulim;돋움/Dotum;바탕/Batang;궁서/Gungsuh;Arial/Arial;Comic Sans MS/Comic Sans MS;Courier New/Courier New;Georgia/Georgia;Lucida Sans Unicode/Lucida Sans Unicode;Tahoma/Tahoma;Times New Roman/Times New Roman;MS Mincho/MS Mincho;Trebuchet MS/Trebuchet MS;Verdana/Verdana',
+        font_defaultLabel: '맑은 고딕/Malgun Gothic',
+        fontSize_defaultLabel: '12',
+        skin: 'Icy Orange',
+        language: 'ko', });
+
