256d1972d5926551c87a413907b4aff6a9cb4f7a
Haeunlee1 <2leehaeun2@gmail.com>
2021-07-29 20:43:18 +0900

    하은

--- src/main/java/com/chairking/poom/board/controller/BoardController.java
+++ src/main/java/com/chairking/poom/board/controller/BoardController.java
@@ -28,7 +28,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
-@RequestMapping("/")
+//@RequestMapping("/")
 @Slf4j
 public class BoardController {
 
