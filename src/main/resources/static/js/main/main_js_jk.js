3cc369b4c56c52b4dafb02b5bf0ebd212dd4e6d9
jinxging <82486622+jinxging@users.noreply.github.com>
2021-07-30 09:49:34 +0900

    getContextPath() 경로수정 -jk

--- src/main/resources/static/js/main/main_js_jk.js
+++ src/main/resources/static/js/main/main_js_jk.js
@@ -1,6 +1,3 @@
-//var contextPath = $('#contextPathHolder').attr('data-contextPath') ? $('#contextPathHolder').attr('data-contextPath') : '';
-var contextPath=[[${#request.getContextPath()}]];
-console.log(contextPath);
 $(document).ready(function(){
  
     // 좌측메뉴바 show/hide 스크립트
@@ -32,7 +29,7 @@ function resize(obj) {
 
 //프로필 부분 쪽지아이콘 클릭스 쪽지 페이지로 이동
 function messageBox(){
-    location.replace("/message?type=receive");
+    location.replace(getContextPath()+"/message?type=receive");
 }
 
 // 프로필 부분 edit버튼 클릭시 정보수정 페이지로 이동 ajax
@@ -42,7 +39,7 @@ function membermodi(){
   	$('.rank').remove();
 
   $.ajax({
-    url:'/member/modiprofile',
+    url: getContextPath()+'/member/modiprofile',
     success:function(data){
         $('#content').html(data)
     },
@@ -55,12 +52,12 @@ function membermodi(){
 }
 
 function mywrite(){
-    location.assign("/mywrite");
+    location.assign(getContextPath()+"/mywrite");
 }
 
 //로그아웃ui 클릭시 로그아웃+index로 이동
 function logOut(){
-    location.replace("/login/logOut");
+    location.replace(getContextPath()+"/login/logOut");
 }
 
 //글쓰기 버튼 클릭시 글작성 페이지로 이동
@@ -70,7 +67,7 @@ function goWriteForm() {
   	$('.feed').css({"background": "#f7f7f7","border-radius":"20px"});
 
   $.ajax({
-    url:'/board/form',
+    url: getContextPath()+'/board/form',
     success:function(data){
         $('.feed').html(data)
     },
