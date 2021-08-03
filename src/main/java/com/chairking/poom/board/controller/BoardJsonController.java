dfe183c58c71060695d8826e9aac535838a31074
ChillingJihan <69563429+jihan-chillin@users.noreply.github.com>
2021-08-03 14:58:21 +0900

    jh : 이미지 정보 가져오기 성공 / 게시글 등록 진행중

--- src/main/java/com/chairking/poom/board/controller/BoardJsonController.java
+++ src/main/java/com/chairking/poom/board/controller/BoardJsonController.java
@@ -1,8 +1,12 @@
 package com.chairking.poom.board.controller;
 
+import com.chairking.poom.board.model.vo.CkFileupload;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
+import org.aspectj.util.FileUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,10 +16,9 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
-import java.io.PrintWriter;
+import java.awt.*;
+import java.io.*;
+import java.net.URLEncoder;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
@@ -112,7 +115,7 @@ public class BoardJsonController {
 //
 //    }
 
-    @RequestMapping(value="/images/ckeditor", method = RequestMethod.POST)
+    @RequestMapping(value = "/images/ckeditor", method = RequestMethod.POST)
     public void imageUpload(HttpServletResponse res, HttpServletRequest req,
                             MultipartHttpServletRequest multireq, @RequestParam MultipartFile upload) throws Exception {
 
@@ -123,31 +126,87 @@ public class BoardJsonController {
         OutputStream out = null;
         PrintWriter writer = null;
 
-        // 나중에 rename할 때 필요한 것들
-        int rndNum = (int) (Math.random() * 100000);
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
-
-        // 폴더 경로 구해보자
+        // 폴더 경로설정
 //        Path path = Paths.get("");
-//        String directoryName = path.toAbsolutePath().normalize().toString(); // C:\Users\JIHAN\IdeaProjects\poom
+//        String directoryName = path.toAbsolutePath().normalize().toString();
         String directoryName = System.getProperty("user.dir"); // C:\Users\JIHAN\IdeaProjects\poom
-//        String folderPath = directoryName + "\\src\\main\\resources\\static\\images\\ckImage\\";
+        String folderPath = directoryName + "\\src\\main\\resources\\static\\uploadCKImage\\";
         //서버 : WEB-INF/classes/template/
 
-        String folderPath = directoryName + "/src/main/resources/ckeditor/images/";
-        System.out.println("resourceLoader로 가져와봄" + folderPath);
 
-        // 파일 원래 이름
-        // 얘를 서버에 전송해줘야 함.
-        String filename = upload.getOriginalFilename();
-//		byte[] bytes = filename.getBytes(StandardCharsets.UTF_8);
+        // 폴더 생성 / 없으면 생성
+        File uploadDir = new File(folderPath);
+        if (!uploadDir.exists()) {
+            uploadDir.mkdirs();
+        }
 
-        // 파일명이랑 확장자 분리
-        String subname = filename.substring(0, filename.lastIndexOf("."));
-        String ext = filename.substring(filename.lastIndexOf(".")).toLowerCase();
-        System.out.println(subname + " : subname, 얘는 확장자 :" + ext);
+        String oriName = upload.getOriginalFilename();
+        String subname = oriName.substring(0, oriName.lastIndexOf("."));
+        String ext = oriName.substring(oriName.lastIndexOf(".")).toLowerCase();
 
+        System.out.println("실제 이름 : " + subname + " / 확장자명 : " + ext);
+
+        // rename 하기
+        int rndNum = (int) (Math.random() * 100000);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+        String renamedFile = sdf.format(System.currentTimeMillis()) + "_" + rndNum + "_" + subname + ext;
+
+        System.out.println(renamedFile);
+
+        // renmaedFile 폴더에 업로드 하기
+        try {
+            upload.transferTo(new File(folderPath + renamedFile));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // 서버로 파일 전송
+        try {
+            String fileUrl = req.getContextPath() + "/ckeditor/fileDownload?fileName=" + renamedFile;
+
+            System.out.println("fileURL : " + fileUrl);
+
+            writer = res.getWriter();
+            writer.println("{\"filename\" : \"" + renamedFile + "\", \"uploaded\" : 1, \"url\":\"" + fileUrl + "\"}");
+            writer.flush();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (writer != null) {
+                writer.close();
+            }
+        }
+    }
+
+
+@RequestMapping("/ckeditor/fileDownload")
+public void ckSubmit(@RequestParam(value = "fileName") String fileName,
+                     HttpServletRequest req, HttpServletResponse res) {
+
+    String directoryName = System.getProperty("user.dir"); // C:\Users\JIHAN\IdeaProjects\poom
+    String folderPath = directoryName + "\\src\\main\\resources\\static\\uploadCKImage\\";
+
+    File file = CkFileupload.getDownloadFile(fileName, folderPath);
+
+    try {
+        byte[] data = FileUtils.readFileToByteArray(file);
+
+//        res.setContentType(CkFileupload.getMediatype(fileName).toString());
+        res.setContentLength(data.length);
+        res.setHeader("Content-Transfer-Encoding", "binary");
+        res.setHeader("Content-Disposition", "attachment; fileName=\""
+                        + URLEncoder.encode(fileName, "UTF-8") +"\";");
+
+        res.getOutputStream().write(data);
+        res.getOutputStream().flush();
+        res.getOutputStream().close();
+
+    } catch (IOException ie) {
+        ie.printStackTrace();
+    }
 
     }
 
 }
+
