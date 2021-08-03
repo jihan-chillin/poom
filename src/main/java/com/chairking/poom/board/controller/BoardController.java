0f056a2e8db69994315872e4d993176c2b0f703f
ChillingJihan <69563429+jihan-chillin@users.noreply.github.com>
2021-08-03 16:08:29 +0900

    jh : boardController 수정

--- src/main/java/com/chairking/poom/board/controller/BoardController.java
+++ src/main/java/com/chairking/poom/board/controller/BoardController.java
@@ -53,62 +53,13 @@ public class BoardController {
 		return "board/board_form";
 	}
 
-	// ckeditor 파일 업로드
-
-
-		//게시글 등록 서비스
-	@PostMapping("/board/insert")
-	public ModelAndView insertBoard(Board board, ModelAndView mv, MultipartFile[] boardImg) throws IOException {
-		board.setMemberId("test");
-		board.setBoardLoc("1");
-		
-		//받아온 게시글 첨부파일을 imgs객체로 저장하기
-		List<BoardImage> imgs=new ArrayList<>();
-		
-		if(boardImg !=null) {
-//			String path=req.getServletContext().getRealPath("/images/board/");
-			String path="";
-			File dir=new File(path);
-			if(!dir.exists()) dir.mkdirs();
-			
-			for(MultipartFile f:boardImg) {
-				if(!f.isEmpty()) {
-					String oriName=f.getOriginalFilename();
-					String ext=oriName.substring(oriName.lastIndexOf("."));
-					SimpleDateFormat sdf=new SimpleDateFormat("yyyyMMdd_HHmmssSSS");
-					int rndNum=(int)(Math.random()*10000);
-					String reName=sdf.format(System.currentTimeMillis())+"_"+rndNum+ext+"_"+board.getBoardCate();
-					
-//					System.out.println(oriName+" -> "+reName);
-					
-					try {
-						f.transferTo(new File(path+reName));
-						BoardImage bi=new BoardImage();
-						bi.setOriginImg(oriName);
-						bi.setRenameImg(reName);
-						System.out.println(bi);
-						imgs.add(bi);
-					}catch(IOException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		}
-		
-		board.setImages(imgs);
-
-		//게시글 등록
-		int result=service.insertBoard(board);
-		
-		if(result!=0) {
-			mv.addObject("board", service.selectBoard(String.valueOf(service.selectBoardNo(board))));
-			mv.setViewName("board/board_view");
-		}else {
-			//에러 처리
-			mv.setViewName("index");
-		}
-		return mv;
-	}
+	// 게시글 등록
+//	@PostMapping("/board/insert")
+//	public ModelAndView insertBoard(HttpServletRequest req, ModelAndView mv,
+//									@RequestParam Map param){
+//
+//
+//	}
 
 	//모든 게시글 리스트 가져오는 서비스
 	@GetMapping("/board/all")
