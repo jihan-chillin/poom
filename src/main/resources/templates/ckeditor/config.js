787730e3cb2de729d19bf0e94fc5771437691b71
ChillingJihan <69563429+jihan-chillin@users.noreply.github.com>
2021-08-10 08:03:15 +0900

    jh : 미리보기 태그 제거 / removetag_content 제거

--- src/main/resources/templates/ckeditor/config.js
+++ src/main/resources/templates/ckeditor/config.js
@@ -5,7 +5,7 @@
 
 CKEDITOR.editorConfig = function( config ) {
 	config.filebrowserUploadMethod='form'; //파일 오류났을때 alert띄워줌
-
+	config.enterMode = CKEDITOR.ENTER_BR; // p태그 자동삽입 방지
 	// 기존 config.js ▼
 	// // Define changes to default configuration here.
 	// // For complete reference see:
